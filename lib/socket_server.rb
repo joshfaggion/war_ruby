04895f01bb100b68202e0bfcc6e2895d4cd6e523
Joshua Faggion <joshua.faggion@gmail.com>
2018-06-06 12:12:51 -0400

    Work on socket server and its spec, add socket_runner file

--- lib/socket_server.rb
+++ lib/socket_server.rb
@@ -4,52 +4,49 @@ require_relative 'game'
 class SocketServer
   attr_reader :player_one, :player_two
   def initialize
-    @player_one="Available"
-    @player_two="Available"
-    @games=[]
-    @steps_completed = 0
+    @games={}
+    @pending_clients = []
   end
+
   def port_number
     2999
   end
+
   def stop
     @server.close if @server
   end
-  def games
-    @games
-  end
+
   def start
     @server = TCPServer.new(port_number)
   end
 
   def accept_new_client(client)
-    if @player_one  == "Available"
-      @player_one = @server.accept_nonblock
+    client_connection = [client , @server.accept_nonblock]
+    @pending_clients.push(client_connection)
+    if @pending_clients.size.odd?
+      client_connection[1].puts "Welcome, no other player is available to battle yet. We will continue to search."
     else
-      @player_two = @server.accept_nonblock
+      client_connection[1].puts "Welcome, a player is available for you to fight!"
     end
   rescue IO::WaitReadable, Errno::EINTR
     puts "No client is available my lord!"
   end
-  
+
   def create_game_if_possible
-    if @player_one == "Available" || @player_two == "Available"
-      return false
-    else
+    if @pending_clients.size > 1
       game = Game.new()
-      @games.push(game)
-      @steps_completed += 1
+      game.begin_game
+      @games.store(game, @pending_clients.shift(2))
+      @games.values.last[0][1].puts "This war is ready to commence, are you ready to play?"
+      @games.values.last[1][1].puts "This war is ready to commence, are you ready to play?"
+    else
+      return false
     end
   end
 
-  def prepare_player
-    @player_one.puts "Prepare for war, my lord!"
-    @player_two.puts "Prepare for war, my lord!"
-    @steps_completed += 1
-  end
-  def ready_to_play?(delay=0.2)
+  def ready_to_play?(delay=0.1, game_id)
     sleep(delay)
-    responses = [@player_one.read_nonblock(1000).chomp, @player_two.read_nonblock(1000).chomp]
+    responses = [@games.values[game_id][0][1].read_nonblock(1000).chomp, @games.values[game_id][1][1].read_nonblock(1000).chomp]
     if responses[0] == 'yes' && responses[1] == 'yes'
       true
     else
@@ -57,5 +54,38 @@ class SocketServer
     end
   rescue IO::WaitReadable
     return false
+  end
+  def cards_in_hands(game_id)
+    # Fill with code.
+    game = @games.keys[game_id]
+    first_client = @games.values[game_id][0][1]
+    second_client = @games.values[game_id][1][1]
+    first_client.puts "You have #{game.player_one.cards_left} cards left in your hand."
+    second_client.puts "You have #{game.player_two.cards_left} cards left in your hand."
+  end
+  def run_round(game_id)
+    game = @games.keys[game_id]
+    first_client = @games.values[game_id][0][1]
+    second_client = @games.values[game_id][1][1]
+    results = game.run_round(false)
+    first_client.puts results
+    second_client.puts results
+  end
+
+  def number_of_games
+    @games.keys.count
+  end
+
+  def find_game(game)
+    @games.keys[game]
+  end
+
+  def setting_player_hand(game_id, cards, player)
+    game = find_game(game_id)
+    if player == 'Player One'
+      game.player_one.set_hand(cards)
+    else
+      game.player_two.set_hand(cards)
+    end
   end
 end
--- spec/socket_server_spec.rb
+++ spec/socket_server_spec.rb
@@ -12,11 +12,11 @@ class MockSocketClient
     @socket.puts(input)
   end
 
-  def take_in_output(delay=0.4)
+  def take_in_output(delay=0.1)
     sleep(delay)
     @output = @socket.read_nonblock(2999)
   rescue IO::WaitReadable
-    @output=''
+    @output='No output to take.'
   end
 
   def close_socket
@@ -47,27 +47,27 @@ describe '#socket_server' do
     @clients.push(client1)
     @server.accept_new_client("Player 1")
     @server.create_game_if_possible
-    expect(@server.games.count).to be 0
+    expect(@server.number_of_games).to be 0
     client2 = MockSocketClient.new(@server.port_number)
     @clients.push(client2)
     @server.accept_new_client("Player 2")
     @server.create_game_if_possible
-    expect(@server.games.count).to be 1
+    expect(@server.number_of_games).to be 1
   end
+
   # Write other tests and test other things.
-  it 'tells the player to prepare for war!' do
+  it 'tells the player welcome' do
     @server.start
     client1 = MockSocketClient.new(@server.port_number)
     @clients.push(client1)
     @server.accept_new_client("Player 1")
-    @server.create_game_if_possible
     client2 = MockSocketClient.new(@server.port_number)
     @clients.push(client2)
     @server.accept_new_client("Player 2")
-    @server.prepare_player()
-    expect(client1.take_in_output.chomp).to eq "Prepare for war, my lord!"
-    expect(client2.take_in_output.chomp).to eq "Prepare for war, my lord!"
+    expect(client1.take_in_output).to eq "Welcome, no other player is available to battle yet. We will continue to search.\n"
+    expect(client2.take_in_output).to eq "Welcome, a player is available for you to fight!\n"
   end
+
     #  Make sure the next round isn't played until both clients say they are ready to play
   it 'should say the game is not ready to be played until set to be ready' do
     @server.start
@@ -78,12 +78,112 @@ describe '#socket_server' do
     client2 = MockSocketClient.new(@server.port_number)
     @clients.push(client2)
     @server.accept_new_client("Player 2")
-    @server.prepare_player
     @server.create_game_if_possible
-    expect(@server.ready_to_play?).to eq false
+    game = 0
+    expect(@server.ready_to_play?(game)).to eq false
     client1.enter_input('yes')
     client2.enter_input('yes')
-    expect(@server.ready_to_play?).to eq true
+    expect(@server.ready_to_play?(game)).to eq true
   end
+
   # What other tests?
+  it 'tells the third player to wait' do
+    @server.start
+    client1 = MockSocketClient.new(@server.port_number)
+    @clients.push(client1)
+    @server.accept_new_client("Player 1")
+    client2 = MockSocketClient.new(@server.port_number)
+    @clients.push(client2)
+    @server.accept_new_client("Player 2")
+    client3 = MockSocketClient.new(@server.port_number)
+    @clients.push(client3)
+    @server.accept_new_client("Player 3")
+    @server.create_game_if_possible
+    expect(client3.take_in_output).to eq "Welcome, no other player is available to battle yet. We will continue to search.\n"
+  end
+
+  context 'when running games' do
+    before (:each) do
+      @server.start
+      client1 = @clients[0]
+      client2 = @clients[1]
+      client3 = MockSocketClient.new(@server.port_number)
+      @clients.push(client3)
+      @server.accept_new_client("Player 3")
+      client4 = MockSocketClient.new(@server.port_number)
+      @clients.push(client4)
+      @server.accept_new_client("Player 4")
+    end
+
+    it 'returns the players card count of the first game' do
+     client1 = @clients[0]
+     client2 = @clients[1]
+     game = 0
+     @server.create_game_if_possible
+     client1.take_in_output
+     @server.cards_in_hands(game)
+     expect(client1.take_in_output).to eq ("You have 26 cards left in your hand.\n")
+    end
+   # Should this run a round?
+    it 'should return the cards left for a second game' do
+      client1 = @clients[0]
+      client2 = @clients[1]
+      @server.create_game_if_possible
+      client3 = MockSocketClient.new(@server.port_number)
+      @clients.push(client3)
+      @server.accept_new_client("Player 3")
+      client4 = MockSocketClient.new(@server.port_number)
+      @clients.push(client4)
+      @server.accept_new_client("Player 4")
+      @server.create_game_if_possible
+      game = 1
+      client4.take_in_output
+      @server.cards_in_hands(game)
+      expect(client4.take_in_output).to eq ("You have 26 cards left in your hand.\n")
+    end
+    it 'should return the proper output for player one winning a round' do
+      client1 = @clients[0]
+      client2 = @clients[1]
+      @server.create_game_if_possible
+      client1.take_in_output
+      client2.take_in_output
+      game_id = 0
+      game = @server.find_game(game_id)
+      game.player_one.set_hand([PlayingCard.new("10", "Spades")])
+      game.player_two.set_hand([PlayingCard.new("4", "Clubs")])
+      @server.run_round(game_id)
+      expect(client1.take_in_output).to eq ("Player One took the 10 of Spades, and the 4 of Clubs!\n")
+      expect(client2.take_in_output).to eq ("Player One took the 10 of Spades, and the 4 of Clubs!\n")
+    end
+    # Test the whole starting process in one big shablooey.
+    it 'should run every part up to the second round' do
+      client1 = @clients[0]
+      client2 = @clients[1]
+      game_id = 0
+       expect(client1.take_in_output).to eq "Welcome, no other player is available to battle yet. We will continue to search.\n"
+       expect(client2.take_in_output).to eq "Welcome, a player is available for you to fight!\n"
+      @server.create_game_if_possible
+       expect(client1.take_in_output).to eq "This war is ready to commence, are you ready to play?\n"
+       expect(client2.take_in_output).to eq "This war is ready to commence, are you ready to play?\n"
+       expect(@server.ready_to_play?(game_id)).to eq false
+      client1.enter_input('yes')
+      client2.enter_input('yes')
+       expect(@server.ready_to_play?(game_id)).to eq true
+      @server.setting_player_hand(game_id, [PlayingCard.new("Queen", "Diamonds")], 'Player One')
+      @server.setting_player_hand(game_id, [PlayingCard.new("Jack", "Spades")], 'Player Two')
+      @server.run_round(game_id)
+       expect(client2.take_in_output).to eq ("Player One took the Queen of Diamonds, and the Jack of Spades!\n")
+       expect(client1.take_in_output).to eq ("Player One took the Queen of Diamonds, and the Jack of Spades!\n")
+       expect(@server.ready_to_play?(game_id)).to eq false
+      client1.enter_input('yes')
+      client2.enter_input('yes')
+       expect(@server.ready_to_play?(game_id)).to eq true
+      @server.setting_player_hand(game_id, [PlayingCard.new("King", "Diamonds")], 'Player One')
+      @server.setting_player_hand(game_id,[PlayingCard.new("2", "Spades")], 'Player Two')
+      @server.run_round(game_id)
+       expect(client1.take_in_output).to eq ("Player One took the King of Diamonds, and the 2 of Spades!\n")
+    end
+    # Test winning the game possibly?
+    # Test two games going on at the same time.
+  end
 end
