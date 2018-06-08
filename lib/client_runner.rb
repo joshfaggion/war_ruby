a986192106c6174507da904c8b72a719689bdc48
Joshua Faggion <joshua.faggion@gmail.com>
2018-06-08 13:08:03 -0400

    Client runner set up

---
+++ lib/client_runner.rb
@@ -0,0 +1,43 @@
+require 'socket'
+require 'pry'
+
+
+loop do
+  begin
+  client = TCPSocket.new '192.168.0.5', 3000
+  puts client.gets
+  puts client.gets
+  answer = ''
+  until answer == "yes\n"
+    answer = gets
+  end
+  client.puts answer
+  while true
+    puts client.gets
+    answer = ''
+    until answer == "yes\n"
+      answer = gets
+     end
+    client.puts answer
+    answer = ''
+    puts client.gets
+    puts client.gets
+  end
+  rescue Errno::ECONNREFUSED
+    puts "Waiting for server to start..."
+    sleep(2)
+  end
+end
+
+#  def run_game(game)
+#     game_id = @games.keys.index(game)
+#     inform_clients_ready(game)
+#     until winner?(game)
+#       ready_to_play?(game)
+#       run_round(game_id)
+#       cards_in_hands(game)
+#       inform_clients_ready(game)
+#       end
+#     end
+#     end_game(game)
+#   end
